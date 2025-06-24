62f39f14a2e5c57f0dc0dfa27ee4bc65827ae029
saebyeokjang <dev.saebyeok@gmail.com>
2025-06-25 03:09:53 +0900

    Initial Xcode project setup

---
+++ WordChainLeague/App/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WordChainLeague
+//
+//  Created by Saebyeok Jang on 6/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ WordChainLeague/App/WordChainLeagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordChainLeagueApp.swift
+//  WordChainLeague
+//
+//  Created by Saebyeok Jang on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct WordChainLeagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
